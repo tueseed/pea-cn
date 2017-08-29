235a389320fdb165aa07624c97b3647d2c431ffe
tueseed <tue_seed@hotmail.com>
2017-08-29 12:14:05 +0700

    Update bot-ssk3.php

--- bot-ssk3.php
+++ bot-ssk3.php
@@ -1,5 +1,5 @@
-<?php
-$access_token = 'uK2r676htODbjL3j7t7cz8N7x52wYxHS7HRY6Y5YT2o8iYJDYipzJaQG7QkYnrF+wlzOKvwCrWlg25b/Et/43ZbsTyQGO9IFqu8IlpmGvTYkPFkT8/cpRpABMVyGWn+eIlJQAzgNhdh2UhWYyFsCvgdB04t89/1O/w1cDnyilFU=';
+<?php ///สมุทรสงคราม3
+$access_token = '21WSV9rt98xFrbN5U8oS7qrkJdNF0N6Bw3zdcItJAuBxChgzBhi7BGoLEUwW9s7dlEaW/gbCEzJh5KoXjH9dFGMXHXlzZ3ufIdG0hXQkx1cVIZIZVE4C8XHu+AU+L7Hg/AKoW2TwmEW2ecC2gYJrhQdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 //ตัวแปรรายการอุปกรณ์
 $pole = "49.เสาคอนกรีต 8 9 9.3 เมตร\n50.เสาคอนกรีต 12 12.2 14 14.3 16 เมตร\n…พิมพ์เลขที่หัวข้อที่ต้องการได้เลยครับ";
