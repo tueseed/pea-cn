3eb074b12f2e478d79c395100d28b41affdc691a
tueseed <tue_seed@hotmail.com>
2017-08-30 23:27:04 +0700

    Update bot-s5.php

--- bot-s5.php
+++ bot-s5.php
@@ -1,5 +1,5 @@
-<?php
-$access_token = 'uK2r676htODbjL3j7t7cz8N7x52wYxHS7HRY6Y5YT2o8iYJDYipzJaQG7QkYnrF+wlzOKvwCrWlg25b/Et/43ZbsTyQGO9IFqu8IlpmGvTYkPFkT8/cpRpABMVyGWn+eIlJQAzgNhdh2UhWYyFsCvgdB04t89/1O/w1cDnyilFU=';
+<?php //กฟต.1-5
+$access_token = 'E8D7fUPBCWSOyT5dIybztBaCk3/WjPLQrkUmLsmKReesLISEE0xZ3af69fLmRlsC6g8kGbRuHg6s9uSrZjC9DM92cVf9AjSRIYZX4kWSYaN622Q+0rDaqbtYNV8Jr8gNFKB707StJhv8LkAO09SxJAdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 //ตัวแปรรายการอุปกรณ์
 $pole = "49.เสาคอนกรีต 8 9 9.3 เมตร\n50.เสาคอนกรีต 12 12.2 14 14.3 16 เมตร\n…พิมพ์เลขที่หัวข้อที่ต้องการได้เลยครับ";
