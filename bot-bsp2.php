238b6bc93c00167aaa5f7bfbca4cc6ca02eebde7
tueseed <tue_seed@hotmail.com>
2017-08-31 13:30:26 +0700

    Update bot-bsp2.php

--- bot-bsp2.php
+++ bot-bsp2.php
@@ -1,5 +1,5 @@
-<?php
-$access_token = 'uK2r676htODbjL3j7t7cz8N7x52wYxHS7HRY6Y5YT2o8iYJDYipzJaQG7QkYnrF+wlzOKvwCrWlg25b/Et/43ZbsTyQGO9IFqu8IlpmGvTYkPFkT8/cpRpABMVyGWn+eIlJQAzgNhdh2UhWYyFsCvgdB04t89/1O/w1cDnyilFU=';
+<?php //บางสะพาน2
+$access_token = 'XGe4hcj5NDJlqcPJsS6QOhgjrCS5IFkbZJ/z43o8O8ZjT+ao6lPtbPLA4fyNTzE7KMQBComYeOLs3L8eIDSIGoJdqFVr3arGbVSMcbz6/v0ZPdRbApmP79DqPVvvjROVBFMWKXl8Jl6U7NnYnB47CAdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 //ตัวแปรรายการอุปกรณ์
 $pole = "49.เสาคอนกรีต 8 9 9.3 เมตร\n50.เสาคอนกรีต 12 12.2 14 14.3 16 เมตร\n…พิมพ์เลขที่หัวข้อที่ต้องการได้เลยครับ";
