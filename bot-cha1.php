a8739a856d46d6085177e8aa6048ad15586653b1
tueseed <tue_seed@hotmail.com>
2017-08-31 10:43:43 +0700

    Update bot-cha1.php

--- bot-cha1.php
+++ bot-cha1.php
@@ -1,5 +1,5 @@
-<?php
-$access_token = 'uK2r676htODbjL3j7t7cz8N7x52wYxHS7HRY6Y5YT2o8iYJDYipzJaQG7QkYnrF+wlzOKvwCrWlg25b/Et/43ZbsTyQGO9IFqu8IlpmGvTYkPFkT8/cpRpABMVyGWn+eIlJQAzgNhdh2UhWYyFsCvgdB04t89/1O/w1cDnyilFU=';
+<?php //ชะอำ1
+$access_token = 'NjK67lT24jce2ewDgnUt5jnMeSr8qwy6F4U7PLMulVNLk3huqs6TXgVLMB27wVfcyccmwVq40NOC5WOCW7Nl4Yh3vBNF4XCAMz6nvQ3EFqh50uPDfiIXPaO/khbeTADkQ/DQsSeUtkzPUYB0an2hSwdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 //ตัวแปรรายการอุปกรณ์
 $pole = "49.เสาคอนกรีต 8 9 9.3 เมตร\n50.เสาคอนกรีต 12 12.2 14 14.3 16 เมตร\n…พิมพ์เลขที่หัวข้อที่ต้องการได้เลยครับ";
