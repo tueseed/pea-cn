21d39bf828b35912ffaa82231921060d90f6d915
tueseed <tue_seed@hotmail.com>
2017-08-31 14:03:28 +0700

    Update bot-rng1.php

--- bot-rng1.php
+++ bot-rng1.php
@@ -1,5 +1,5 @@
-<?php
-$access_token = 'uK2r676htODbjL3j7t7cz8N7x52wYxHS7HRY6Y5YT2o8iYJDYipzJaQG7QkYnrF+wlzOKvwCrWlg25b/Et/43ZbsTyQGO9IFqu8IlpmGvTYkPFkT8/cpRpABMVyGWn+eIlJQAzgNhdh2UhWYyFsCvgdB04t89/1O/w1cDnyilFU=';
+<?php //ระนอง1
+$access_token = 'OBtetGKO3ogkCqLjXKMOtB0WupWW5ItN3b7atiT/6Eb69JlT60NIQtLPv+X4CryggTdLPWJx+vn+kcEghIappD0VvC+lzkpZN9d5Otpb7m89BDvXDgeG9E/Ew+VLVkzfP2cV/1jVrReDe9lE5H7LGwdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 //ตัวแปรรายการอุปกรณ์
 $pole = "49.เสาคอนกรีต 8 9 9.3 เมตร\n50.เสาคอนกรีต 12 12.2 14 14.3 16 เมตร\n…พิมพ์เลขที่หัวข้อที่ต้องการได้เลยครับ";
