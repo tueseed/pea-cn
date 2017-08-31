03988eccea2af360c2505657af60a9376a20129d
tueseed <tue_seed@hotmail.com>
2017-08-31 10:35:21 +0700

    Update bot-pbr4.php

--- bot-pbr4.php
+++ bot-pbr4.php
@@ -1,5 +1,5 @@
-<?php
-$access_token = 'uK2r676htODbjL3j7t7cz8N7x52wYxHS7HRY6Y5YT2o8iYJDYipzJaQG7QkYnrF+wlzOKvwCrWlg25b/Et/43ZbsTyQGO9IFqu8IlpmGvTYkPFkT8/cpRpABMVyGWn+eIlJQAzgNhdh2UhWYyFsCvgdB04t89/1O/w1cDnyilFU=';
+<?php //พบ4
+$access_token = 'FdKbSCajtmc6klC3h5vbed00hJIWBtlV5YdkHmB0/Ijvk6PMLy1ihwH72Z8wzbfs0unNG4+44rKaWckH/y3QKsfBYEVCSpWhsm9gHRxDEKxc8c5O0Htcqwx3se0bCyMbEv7+ABbSDTViFMxK0Y7HCAdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 //ตัวแปรรายการอุปกรณ์
 $pole = "49.เสาคอนกรีต 8 9 9.3 เมตร\n50.เสาคอนกรีต 12 12.2 14 14.3 16 เมตร\n…พิมพ์เลขที่หัวข้อที่ต้องการได้เลยครับ";
