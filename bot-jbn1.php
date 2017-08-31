41e2eaa10820a4359f796c7789908662aa47b948
tueseed <tue_seed@hotmail.com>
2017-08-31 09:07:16 +0700

    Update bot-jbn1.php

--- bot-jbn1.php
+++ bot-jbn1.php
@@ -1,5 +1,5 @@
-<?php
-$access_token = 'uK2r676htODbjL3j7t7cz8N7x52wYxHS7HRY6Y5YT2o8iYJDYipzJaQG7QkYnrF+wlzOKvwCrWlg25b/Et/43ZbsTyQGO9IFqu8IlpmGvTYkPFkT8/cpRpABMVyGWn+eIlJQAzgNhdh2UhWYyFsCvgdB04t89/1O/w1cDnyilFU=';
+<?php //จอมบึง1
+$access_token = '0gMvoXByvHEd+e/OufBSO2U03f0zOOMq50YraCkNYgSxmsc/VWJnyg6JDxhZN7h/zHaOBJqht7FkVdGnyLcX3d59hWFwn/P0X3yjZpFM/07kFvwY7q3/RTYRFEeWvcV3IzJ9+9Pa9RNinGDzLpzPEgdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 //ตัวแปรรายการอุปกรณ์
 $pole = "49.เสาคอนกรีต 8 9 9.3 เมตร\n50.เสาคอนกรีต 12 12.2 14 14.3 16 เมตร\n…พิมพ์เลขที่หัวข้อที่ต้องการได้เลยครับ";
