cbd198211418d39ff975ee37493b2ad0f051e966
tueseed <tue_seed@hotmail.com>
2017-08-31 10:55:16 +0700

    Update bot-hua1.php

--- bot-hua1.php
+++ bot-hua1.php
@@ -1,5 +1,5 @@
-<?php
-$access_token = 'uK2r676htODbjL3j7t7cz8N7x52wYxHS7HRY6Y5YT2o8iYJDYipzJaQG7QkYnrF+wlzOKvwCrWlg25b/Et/43ZbsTyQGO9IFqu8IlpmGvTYkPFkT8/cpRpABMVyGWn+eIlJQAzgNhdh2UhWYyFsCvgdB04t89/1O/w1cDnyilFU=';
+<?php //หัวหิน1
+$access_token = 'cbCGR9sig0dVhXO+dOM68r5J4TILnmRG4FvuwXIbwsNVglztrDkRKaHyVwOtaxs/6Ffq0WmWVvdo1hnBdTH0Guz40gnMKTUdDaSdsl2yXFPak1sVXZEwDavGX3hs53i7JbV6yuGzmCAAGGGjYvnRwgdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 //ตัวแปรรายการอุปกรณ์
 $pole = "49.เสาคอนกรีต 8 9 9.3 เมตร\n50.เสาคอนกรีต 12 12.2 14 14.3 16 เมตร\n…พิมพ์เลขที่หัวข้อที่ต้องการได้เลยครับ";
