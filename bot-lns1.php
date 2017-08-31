6b9df0d726c5e2bef2a1bc30a4a2adb734913434
tueseed <tue_seed@hotmail.com>
2017-08-31 14:12:52 +0700

    Update bot-lns1.php

--- bot-lns1.php
+++ bot-lns1.php
@@ -1,5 +1,5 @@
-<?php
-$access_token = 'uK2r676htODbjL3j7t7cz8N7x52wYxHS7HRY6Y5YT2o8iYJDYipzJaQG7QkYnrF+wlzOKvwCrWlg25b/Et/43ZbsTyQGO9IFqu8IlpmGvTYkPFkT8/cpRpABMVyGWn+eIlJQAzgNhdh2UhWYyFsCvgdB04t89/1O/w1cDnyilFU=';
+<?php //หลังสวน1
+$access_token = '6KL2hDL7hD2pP/aCtuFJpgXCskF0IewACtcbnliOYNeahVViXAd1LEwQ4Bq9OIAQoG7Ecx5DIQMNF4nPtXJGwOHxZ38T3Nlu3n9ee4iSXO8HLxWzVpQu/KvuyMS8tOQE7zIwZ59Ezw3DuVAoFja0gwdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 //ตัวแปรรายการอุปกรณ์
 $pole = "49.เสาคอนกรีต 8 9 9.3 เมตร\n50.เสาคอนกรีต 12 12.2 14 14.3 16 เมตร\n…พิมพ์เลขที่หัวข้อที่ต้องการได้เลยครับ";
